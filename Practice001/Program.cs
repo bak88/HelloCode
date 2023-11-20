cad1f244d8f23c91d228d15e1f5bfde93b88bb5a
bak88 <dimbak1990@gmail.com>
2023-11-20 22:13:48 +0300

    Practice002

---
+++ Practice001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите трехзнаное число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+while (n < 100 || n > 999)
+{
+    System.Console.WriteLine("Ошибка !\nВведите трехзнаное число: ");
+    n = int.Parse(Console.ReadLine()!);
+    }
+
+    int n1 = n / 100;
+    int n3 = n % 10;
+    System.Console.WriteLine($"{n1}{n3}" );
