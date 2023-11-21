a92ba793c3f448fffc6f45648468854efd253258
bak88 <dimbak1990@gmail.com>
2023-11-21 18:47:54 +0300

    DZ002

---
+++ DZ002/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 1
+
+// Console.Clear();
+// System.Console.WriteLine("Введите число");
+// int firstNumber = int.Parse(Console.ReadLine()!);
+
+// if (firstNumber % 7 == 0 && firstNumber % 23 == 0)
+// {
+//     System.Console.WriteLine($"Да {firstNumber} кратно");    
+// } 
+// else
+// {
+//     System.Console.WriteLine($"Нет, {firstNumber} не кратно");
+// }
+
+// Задача 2
+// Console.Clear();
+// System.Console.WriteLine("Введите координату х: ");
+// int x = int.Parse(Console.ReadLine()!);
+// System.Console.WriteLine("Введите координату y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// while (x == 0 || y == 0)
+// {
+//     System.Console.WriteLine("Введите координату не равную 0!");
+//     System.Console.WriteLine();
+//     System.Console.WriteLine("Введите координату х: ");
+//     x = int.Parse(Console.ReadLine()!);
+//     System.Console.WriteLine("Введите координату y: ");
+//     y = int.Parse(Console.ReadLine()!);
+// }
+// if (x > 0 && y > 0)
+// {
+//     System.Console.WriteLine("Координаты принадлеат 1ой четверти");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     System.Console.WriteLine("Координаты принадлеат 2ой четверти");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     System.Console.WriteLine("Координаты принадлеат 3ей четверти");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     System.Console.WriteLine("Координаты принадлеат 4ой четверти");
+// }
+
+// //Задача 3
+// Console.Clear();
+// System.Console.WriteLine("Введите число от 10 до 99");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n < 10 || n > 99)
+// {
+//     System.Console.WriteLine("Ошибка!\nВведите число от 10 до 99");
+//     n = int.Parse(Console.ReadLine()!);
+// }
+//  int n2 = n / 10;
+//  int n3 = n % 10;
+
+//  if (n2 < n3)
+//  {
+//     System.Console.WriteLine(n3);
+//  }
+//  else if (n2 > n3)
+//  {
+//     System.Console.WriteLine(n2);
+//  }
+//  else
+//  {
+//     System.Console.WriteLine(n2);
+//  }
+
+// Задача 4
