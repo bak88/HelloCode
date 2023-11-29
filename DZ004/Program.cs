53bb22fdc6b28aa395b48c3a495138692048fc65
bak88 <dimbak1990@gmail.com>
2023-11-29 22:39:30 +0300

    Examples013_Recursion

--- DZ004/Program.cs
+++ DZ004/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Clear();
+
+
+while (true)
+{
+    System.Console.WriteLine("Введите число: ");
+    string n = Console.ReadLine()!;
+        
+    if (n == "q")
+    {
+        break;
+    }
+    
+}
