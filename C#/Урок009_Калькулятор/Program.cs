eb1ca532fb3da2de7edba1975de176de4038627f
bak88 <dimbak1990@gmail.com>
2023-12-09 21:48:05 +0300

    Урок009_Калькулятор

---
+++ C#/Урок009_Калькулятор/Program.cs
@@ -0,0 +1,97 @@
+﻿Console.Clear();
+
+
+
+
+// Калькуляьор через switch
+
+System.Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите операцию '+', '-', '*', '/' ");
+string action = Console.ReadLine()!;
+System.Console.WriteLine("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+double result;
+
+switch (action)
+{
+    case "+":
+        {
+            result = a + b;
+            System.Console.WriteLine(result);
+        }
+        break;
+    case "-":
+        {
+            result = a - b;
+            System.Console.WriteLine(result);
+        }
+        break;
+    case "*":
+        {
+            result = a * b;
+            System.Console.WriteLine(result);
+        }
+        break;
+    case "/":
+        {
+            if (b == 0)
+            {
+                System.Console.WriteLine("Ошибка!!! Делить на ноль нельзя");
+            }
+            else
+            {
+                result = a / b;
+                System.Console.WriteLine(result);
+            }
+        }
+        break;
+    default:
+        {
+            System.Console.WriteLine("Ошибка!!! Такой операции нет");
+        }
+        break;
+}
+
+
+
+
+// // Калькуляьор через if, else
+
+// System.Console.WriteLine("Введите первое число: ");
+// double a = double.Parse(Console.ReadLine()!);
+// System.Console.WriteLine("Введите операцию '+', '-', '*', '/' ");
+// string action = Console.ReadLine();
+// System.Console.WriteLine("Введите второе число: ");
+// double b = Convert.ToDouble(Console.ReadLine());
+// double result;
+
+
+// if (action == "+")
+// {
+//    result = a + b;
+//    System.Console.WriteLine(result);
+// }
+// else if (action == "-")
+// {
+//     result = a - b;
+//    System.Console.WriteLine(result);
+// }
+// else if (action == "*")
+// {
+//     result = a * b;
+//    System.Console.WriteLine(result);
+// }
+// else if (action == "/")
+// {
+//     if(b == 0)
+//     {
+//         System.Console.WriteLine("Ошибка!!! На ноль делить нельзя");
+//     }
+//     else
+//     {
+//          result = a / b;
+//         System.Console.WriteLine(result);
+//     }
+
+// }
---
+++ Буткемп_Программирование/Урок 8_Быстрая_сортировка/Program.cs
@@ -0,0 +1,48 @@
+﻿void inputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21); // [-20; 20]
+}
+
+
+int[] quickSort(int[] array, int leftIndex, int rightIndex)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}] ({leftIndex}, {rightIndex})");
+    int i = leftIndex, j = rightIndex, pivot = array[leftIndex];
+    while (i <= j) 
+    {
+        while (array[i] < pivot) 
+        {
+            i++; 
+        }
+        while (array[j] > pivot) 
+        {
+            j--; 
+        }
+        if (i <= j) 
+        {
+            int temp = array[i]; 
+            array[i] = array[j];
+            array[j] = temp;
+            i++; 
+            j--; 
+        }
+    }
+    if (leftIndex < j)
+        quickSort(array, leftIndex, j);
+    
+    if (i < rightIndex) 
+        quickSort(array, i, rightIndex); 
+    
+    return array;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+//int[] array = {-8, 19, 6, 12, 15};
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", quickSort(array, 0, array.Length - 1))}]");
