11ea179d62f9fed69b846d99891040589ebcdf55
bak88 <dimbak1990@gmail.com>
2024-01-15 11:01:55 +0300

    Сортировка файлов

--- C#/Семинары/Practice001_02/Program.cs
+++ C#/Семинары/Practice001_02/Program.cs
@@ -7,7 +7,7 @@ while (n < 100 || n > 999)
 {
     System.Console.WriteLine("Ошибка !\nВведите трехзнаное число: ");
     n = int.Parse(Console.ReadLine()!);
-    }
+}
 
     int n1 = n / 100;
     int n3 = n % 10;
